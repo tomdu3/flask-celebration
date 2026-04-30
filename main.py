c785f90eb80195196e31dfb93e80ed01fce29597
Tomislav Dukez <tomdu3@ymail.com>
2026-04-30 10:35:13 +0100

    add initial flask app

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    context = {
+        "title": "Hello, Sumi!",
+        "message": "This is a test.",
+        "name": "Sumi",
+        "age": 25,
+        "is_student": True,
+        "hobbies": ["reading", "coding", "playing games"],
+        "friends": ["Tom", "Jerry", "Harry"],
+    }
+
+    return render_template("index.html", context=context)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
