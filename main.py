dc7da3474f4225bb46cdaa359ea0ca6a800571df
ci-sumi <tharayilsurendransumi@gmail.com>
2026-05-02 13:36:05 +0100

    Jinga with Tile to capitalize first charcter in a word

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def home():
         "friends": ["Tom", "Jerry", "Harry"],
     }
     Heading = "Welcome to Flask!"
-    My_stuff="<strong>Flask is fun!</strong>"
+    My_stuff="Flask is fun!"
 
     return render_template("index.html", context=context, Heading=Heading,
                            My_stuff=My_stuff)
