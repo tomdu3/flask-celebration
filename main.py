b6afd5d4da4c7811eceff252164def597a0d1852
ci-sumi <tharayilsurendransumi@gmail.com>
2026-05-02 20:01:28 +0100

    Add route and function for getting gender and age from API's

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
@@ -23,7 +24,17 @@ def home():
 
 @app.route("/guess/<name>")
 def guess(name):
-    return render_template("guess.html",gues_name=name)
+    gender_url=f"https://api.genderize.io?name={name}"
+    age_url=f"https://api.agify.io?name={name}"
+    get_age_url_response=requests.get(age_url)
+    get_age_url_data=get_age_url_response.json()
+    age=get_age_url_data["age"]
+    get_gender_response=requests.get(gender_url)
+    get_gender_data=get_gender_response.json()
+    gender=get_gender_data["gender"]
+    
+    return render_template("guess.html",gues_name=name,gender=gender,age=age)
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
